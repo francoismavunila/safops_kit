2617e413f17f26dedac2fc73d9910fd7723de322
francoismavunila <mavunila002@gmail.com>
2023-09-28 05:30:37 +0000

    update

--- dao/eitDAO.js
+++ dao/eitDAO.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 require('dotenv').config()
+console.log(process.env.DATABASE_URL)
 const connection = mysql.createConnection(process.env.DATABASE_URL)
 
 // DAO functions

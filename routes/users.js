077a5156045b94eda97c690f4582d6af4af7b418
francoismavunila <mavunila002@gmail.com>
2023-09-28 22:29:59 +0000

    update

--- routes/users.js
+++ routes/users.js
@@ -4,20 +4,35 @@ const eitController = require('../controllers/eitController');
 const authenticate = require('../controllers/authentication')
 const validateToken = require('../controllers/validate')
 
+function checkEmailDomain(email) {
+    const domain = "@meltwater.org";
+  
+    // Check if the email ends with the specified domain
+    const isDomainMatch = email.endsWith(domain);
+    console.log("domain match says "+isDomainMatch)
+  
+    return isDomainMatch;
+  }
 // Define routes for EITs
 router.get('/eits', eitController.getAllEITs);
 router.post('/eits', eitController.createEIT);
 
 router.post('/signin',(req,res)=>{
+    
     var {email,password} = req.body
-    authenticate(email,password,(error, response)=>{
-        console.log("called")
-        if(error){
-            res.status(400).send({"error":error});
-        }else{
-            res.status(200).send(response)
-        }
-    })
+    if(checkEmailDomain(email)){
+        authenticate(email,password,(error, response)=>{
+            console.log("called")
+            if(error){
+                res.status(400).send({"error":error});
+            }else{
+                res.status(200).send(response)
+            }
+        })
+    }else{
+        res.status(401).send({message:"email not from our domain"})
+    }
+
 })
 
 router.get('/protected', validateToken, (req, res) => {
